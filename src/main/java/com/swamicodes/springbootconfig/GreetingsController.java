fd54301f4cd1845be64ac14b6125daafb2b666bb
swami <swami.gms@gmail.com>
2021-02-18 23:05:04 +0530

    Reading properites using ConfigurationProperties & Configuration annotations

--- src/main/java/com/swamicodes/springbootconfig/GreetingsController.java
+++ src/main/java/com/swamicodes/springbootconfig/GreetingsController.java
@@ -1,35 +1,43 @@
 package com.swamicodes.springbootconfig;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingsController {
-	
+
 	@Value("${my.greetings}")
 	private String myGreetings;
-	
+
 	@Value("Some Static Message")
 	private String appName;
-	
+
 	@Value("${app.description: Default Value}")
 	private String appDescription;
-	
+
 	@Value("${my.list.values}")
 	private List<String> listValues;
-	
-	@Value("#{${dbvalues}}")
+
+	@Value("#{${db.connection}}")
 	private Map<String, String> dbValues;
-	
+
+	@Autowired
+	private DbSettings dbSettings;
+
 	@GetMapping("/greetings")
 	public String greetings() {
-	
+
 		return myGreetings + appName + appDescription + listValues + dbValues;
+	}
+
+	@GetMapping("/db")
+	public String dbSettings() {
+		return dbSettings.getConnection() + dbSettings.getHost() + dbSettings.getPort();
 	}
 
 }
