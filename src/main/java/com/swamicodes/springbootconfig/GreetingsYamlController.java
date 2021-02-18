9e73a671bf76fc31be8ea518839b23e93bca6832
swami <swami.gms@gmail.com>
2021-02-18 23:47:39 +0530

    Implementing YAML file for configuration

---
+++ src/main/java/com/swamicodes/springbootconfig/GreetingsYamlController.java
@@ -0,0 +1,48 @@
+package com.swamicodes.springbootconfig;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("yml")
+public class GreetingsYamlController {
+
+
+	@Value("${our.greetings}")
+	private String myGreetings;
+
+	@Value("${service.name}")
+	private String appName;
+
+	@Value("${service.description: Default Value}")
+	private String appDescription;
+
+	@Value("${our.list.values}")
+	private List<String> listValues;
+
+	@Value("#{${database.connection}}")
+	private Map<String, String> dbValues;
+
+	@Autowired
+	private DbSettings dbSettings;
+
+	@GetMapping("/greetings")
+	public String greetings() {
+
+		return myGreetings + appName + appDescription + listValues + dbValues;
+	}
+
+	@GetMapping("/db")
+	public String dbSettings() {
+		return dbSettings.getConnection() + dbSettings.getHost() + dbSettings.getPort();
+	}
+
+
+
+}
